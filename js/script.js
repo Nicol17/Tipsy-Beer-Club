7eaeb19b64480bf4a55696be726d608a6c33ccfc
danilo-mello <dmello19@gmail.com>
2021-02-10 14:48:04 -0800

    Fix add to cart

--- js/script.js
+++ js/script.js
@@ -7,166 +7,114 @@
     });
 })();
 
-
-
-
 const productsDiv = document.querySelector('.products');
 
+async function getBeers() {
 
-const getBeers = async () => {
     const option = { headers: { 'Accept': 'application/json' } };
     const response = await axios.get('https://api.punkapi.com/v2/beers/random', option);
     const beer = response.data[0];
 
     const card = document.createElement('div');
     card.classList.add("product-card");
+    card.id = beer.id;
+    card.innerHTML = `
+                        <div class="product-img">
+                            <img class="card-img" src="${tryImg(beer.image_url)}">
+                        </div>
+                        <div class="product-name">
+                            <p class="card-name">${beer.name}</p>
+                        </div>
+                        <div class="product-info">
+                        <div class="product-info-par">
+                            <div class="product-info-abv">
+                                <p>ABV: ${beer.abv} %</p>
+                            </div>
+                            <div class="product-info-ibu">
+                                <p>IBU: ${beer.ibu}</p>
+                            </div>
+                        </div>
+                        <div class="product-info-description">
+                            <p>${trimDescription(beer.description)}</p>
+                        </div>
+                        </div>  
+                        <div class="product-price">
+                            <p class="card-price">${randomPrice()}</p>
+                        </div>
+                        <button class="add-to-cart">
+                            <p>Add</p>
+                            <i class="flaticon-beer-mug"></i>
+                        </button>
+    `;
+
     productsDiv.appendChild(card);
 
-    const cardImg = document.createElement('div');
-    cardImg.classList.add("product-img");
-    card.appendChild(cardImg);
+    addToCart(beer.id);
 
-    const img = document.createElement('img');
-    cardImg.appendChild(img);
-    var tryImg = beer.image_url;
-    if (tryImg == null) {
-        tryImg = "../img/beer.png";
-    }
-    img.src = tryImg;
+};
 
-    const cardName = document.createElement('div');
-    cardName.classList.add("product-name");
-    card.appendChild(cardName);
-
-    const name = document.createElement('p');
-    cardName.appendChild(name);
-    name.innerText = beer.name;
-
-    const cardInfo = document.createElement('div');
-    cardInfo.classList.add("product-info");
-    card.appendChild(cardInfo);
-
-    const cardPrice = document.createElement('div');
-    cardPrice.classList.add("product-price");
-    card.appendChild(cardPrice);
-
-    const price = document.createElement('p');
-    cardPrice.appendChild(price);
-    price.innerText = "$ " + Number((Math.floor(Math.random() * (1499 - 349 + 1)) + 349) / 100).toFixed(2);
-
-    const addToCartBtn = document.createElement('button');
-    addToCartBtn.classList.add("add-to-cart");
-    card.appendChild(addToCartBtn);
-    (function(){
+function addToCart(idOr) {
 
     const cartBtn = document.querySelectorAll('.add-to-cart');
     
-        cartBtn.forEach(function(btn) {
-            btn.addEventListener('click', function(event){
-    
-                let thumbnail = event.target.parentElement.parentElement.firstChild.firstChild.src;
-                let name = event.target.parentElement.parentElement.firstChild.nextSibling.firstChild.innerText;
-                let price = event.target.parentElement.parentElement.firstChild.nextSibling.nextSibling.nextSibling.innerText
+    cartBtn.forEach(function(btn) {
+        btn.addEventListener('click', function(event){
+
+            let id = event.target.parentElement.parentElement.id;
+            if (id==idOr) {
+                let thumbnail = event.target.parentElement.parentElement.firstChild.nextSibling.firstChild.nextSibling.src;
+                let name = event.target.parentElement.parentElement.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.innerText;
+                let price = event.target.parentElement.parentElement.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.innerText;
                 price = Number(price.slice(2, price.length));
-                
-
-
+                            
                 const beer = {};
                 beer.img = thumbnail;
                 beer.name = name;
                 beer.price = price;
 
-
                 const cartItem = document.createElement('div');
                 cartItem.classList.add('cart-item');
-                cartItem.innerHTML = `<img src="${beer.img}" class="img-fluid rounded-circle" id="item-img" alt="beer image">
-                <div class="item-text">
-                  <p id="cart-item-title" class="font-weight-bold mb-0">${beer.name}</p>
-                  <span>$</span>
-                  <span id="cart-item-price" class="cart-item-price" class="mb-0">${beer.price}</span>
-                </div>
-                <a href="#" id='cart-item-remove' class="cart-item-remove">
-                  <i class="fas fa-trash"></i>
-                </a>`;
+                cartItem.innerHTML = `
+                    <img src="${beer.img}" class="img-fluid rounded-circle" id="item-img" alt="beer image">
+                    <div class="item-text">
+                        <p id="cart-item-title" class="font-weight-bold mb-0">${beer.name}</p>
+                        <span>$</span>
+                        <span id="cart-item-price" class="cart-item-price" class="mb-0">${beer.price}</span>
+                        </div>
+                        <a href="#" id='cart-item-remove' class="cart-item-remove">
+                            <i class="fas fa-trash"></i>
+                        </a>
+                `;
 
                 const cart = document.getElementById('cart');
                 const total = document.querySelector('.cart-total-container');
 
-
                 cart.insertBefore(cartItem, total);
                 alert("Your beer has been poured into the Barrel!");
                 showTotal();
-
-            })
-    
+            }
         })
-    
-
-        function showTotal() {
-            const total = [];
-            const items = document.querySelectorAll('.cart-item-price');
-            items.forEach(function(item){
-                total.push(parseFloat(item.innerText));
-            });
+    })
         
+    function showTotal() {
+        const total = [];
+        const items = document.querySelectorAll('.cart-item-price');
+        items.forEach(function(item){
+            total.push(parseFloat(item.innerText));
+        });
+            
+        const totalPrice = total.reduce(function(total, item){
+            total += item;
+               
+            return total;
+        }, 0)
 
-            const totalPrice = total.reduce(function(total, item){
-                total += item;
-                
-                return total;
-            }, 0)
+        document.getElementById('cart-total').textContent = totalPrice;
+        document.querySelector('.item-total').textContent = totalPrice;
+        document.querySelector('.item-count').textContent= total.length;
+    }
 
-            console.log(totalPrice);
-
-            document.getElementById('cart-total').textContent = totalPrice;
-            document.querySelector('.item-total').textContent = totalPrice;
-            document.getElementById('item-count').textContent= total.length;
-
-        }
-
-
-    
-    })();
-    
-
-    const buttonText = document.createElement('p');
-    addToCartBtn.appendChild(buttonText);
-    buttonText.innerText = ("Add");
-
-    const buttonIcon = document.createElement('i');
-    addToCartBtn.appendChild(buttonIcon);
-    buttonIcon.classList.add("flaticon-beer-mug");
-
-    const cardInfoPar = document.createElement('div');
-    cardInfoPar.classList.add("product-info-par");
-    cardInfo.appendChild(cardInfoPar);
-
-    const cardInfoAbv = document.createElement('div');
-    cardInfoAbv.classList.add("product-info-abv");
-    cardInfo.appendChild(cardInfoAbv);
-
-    const abv = document.createElement('p');
-    cardInfoAbv.appendChild(abv);
-    abv.innerText = "ABV: " + beer.abv + "%";
-
-    const cardInfoIbu = document.createElement('div');
-    cardInfoIbu.classList.add("product-info-ibu");
-    cardInfo.appendChild(cardInfoIbu);
-
-    const ibu = document.createElement('p');
-    cardInfoIbu.appendChild(ibu);
-    ibu.innerText = "IBU: " + beer.ibu;
-
-    const cardInfoDescription = document.createElement('div');
-    cardInfoDescription.classList.add("product-info-description");
-    cardInfo.appendChild(cardInfoDescription);
-
-    const info = document.createElement('p');
-    cardInfoDescription.appendChild(info);
-    info.innerText = trimDescription(beer.description);
-
-
-};
+}
 
 for (let i=0; i<5; i++){
     getBeers();
@@ -182,5 +130,13 @@ function trimDescription(cardInfoDescription) {
     return cardInfoDescription;
 }
 
+function tryImg(imgurl) {
+    if (imgurl == null) {
+        imgurl = "../img/beer.png";
+    }
+    return imgurl;
+}
 
-
+function randomPrice() {
+    return "$ " + Number((Math.floor(Math.random() * (1499 - 349 + 1)) + 349) / 100).toFixed(2);
+}
